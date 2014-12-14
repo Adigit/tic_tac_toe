b34d5bab0451f8b3e1a316ee8946788860877b80
traveltriangle <adi.anu.ducs@gmail.com>
2014-12-14 16:15:07 +0530

    board class added

---
+++ lib/tic_tac_toe/board.rb
@@ -0,0 +1,70 @@
+module TicTacToe
+  class Board
+  	attr_reader :grid
+    def initialize(input = {})
+      @grid = input.fetch(:grid, default_grid)
+    end
+
+    def formatted_grid
+    	 grid.each do |x|
+    	 	x.each do |_x|
+    	 		if _x.value == ""
+    	 			print " * |" 
+    	 		else
+    	 			print " #{_x.value} |"
+    	 		end
+    	 	end
+    	 	puts "\n____________"
+    	 end
+    end
+
+    def get_cell(x, y)
+  		grid[y][x]
+	end
+
+	def set_cell(x, y, value)
+  		get_cell(x, y).value = value
+	end
+
+	def game_over
+  		return :winner if winner?
+  		return :draw if draw?
+  		false
+	end
+
+	def draw?
+  		grid.flatten.map { |cell| cell.value }.none_empty?
+	end
+
+	def winning_positions
+	  grid + # rows
+	  grid.transpose + # columns
+	  diagonals # two diagonals
+	end
+ 
+	def diagonals
+	  [
+	    [get_cell(0, 0), get_cell(1, 1), get_cell(2, 2)],
+	    [get_cell(0, 2), get_cell(1, 1), get_cell(2, 0)]
+	  ]
+	end
+
+	def winner?
+	  winning_positions.each do |winning_position|
+	    next if winning_position_values(winning_position).all_empty?
+	    return true if winning_position_values(winning_position).all_same?
+	  end
+	  false
+	end
+ 
+	def winning_position_values(winning_position)
+	  winning_position.map { |cell| cell.value }
+	end
+
+	private
+ 
+    def default_grid
+      Array.new(3) { Array.new(3) { Cell.new } }
+    end
+  end
+end
