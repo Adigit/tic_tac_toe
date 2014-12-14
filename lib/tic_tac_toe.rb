e896e1b405d15118b8c84f7ea5dd097c617c649f
traveltriangle <adi.anu.ducs@gmail.com>
2014-12-14 16:16:13 +0530

    load classes

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,10 @@
-require "tic_tac_toe/version"
-
+require_relative "tic_tac_toe/version"
+ 
 module TicTacToe
-  # Your code goes here...
 end
+ 
+require_relative "./tic_tac_toe/cell.rb"
+require_relative "./tic_tac_toe/player.rb"
+require_relative "./tic_tac_toe/game.rb"
+require_relative "./tic_tac_toe/board.rb"
+require_relative "./tic_tac_toe/core_extensions.rb"
